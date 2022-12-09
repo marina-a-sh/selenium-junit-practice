54f7c14e4bc9a7697db3ef26c60179f845ad143e
Marina A. Shaposhnik <marina.shaposhnik@gmail.com>
2022-12-08 16:28:31 -0800

    assume and use internal docker bridge network
    assume that selenoid, browser spun by selenoid, and jenkins container named "jenkinsci" are all connected by docker bridge network (not accessible from host), for example default selenoid bridge network called "selenoid" can be used for this purpose
    as part of "mvn test" flow jetty server is serving static html file on 0.0.0.0:8889 (0.0.0.0 means any IP address for which there is no more specific address); if no jenkins agents are used jetty is running in the jenkinsci container
    from browser in its container html file served in jenkinsci container is reached by http://jenkinsci:8889/[filename] URL thanks to docker bridge network

--- src/test/java/org/groundwork/support/TestContext.java
+++ src/test/java/org/groundwork/support/TestContext.java
@@ -168,8 +168,15 @@ public class TestContext {
         if (getConfig().getEnvironment().equals("local")) {
             return "http://localhost:8889";
         }
-        // selenoid's browser configurations in browsers.json (on linux ~/.aerokube/selenoid/browsers.json)
-        // should have "hosts" : ["host.docker.internal:host-gateway"] added
+        // running from dockerized jenkins (container name: "jenkinsci")
+        // jenkinsci container should be added to docker bridge network (default: "selenoid") with selenoid and browsers
+        if (!getConfig().getHub().contains("localhost")) {
+            return "http://jenkinsci:8889";
+        }
+        // running from IDE, containerized browser should reach localhost:8889 of the host
+        // selenoid's browser configurations in browsers.json should have added
+        // "hosts" : ["host.docker.internal:host-gateway"]
+        // use "--browsers-json [dir_name]/browsers.json --config-dir [dir_name]" of selenoid configuration manager
         return "http://host.docker.internal:8889";
     }
 }
