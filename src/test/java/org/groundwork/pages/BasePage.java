42bfc7efd002185474f3b480c132f3781ddd1e27
Marina A. Shaposhnik <1000324+marina-a-sh@users.noreply.github.com>
2023-04-11 21:59:34 -0700

    add advertisement overlay closure
    In case page takes a lot of time to load completely advertisement overlay is shown and need to be closed.

--- src/test/java/org/groundwork/pages/BasePage.java
+++ src/test/java/org/groundwork/pages/BasePage.java
@@ -1,9 +1,15 @@
 package org.groundwork.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.groundwork.support.TestContext.getConfig;
 import static org.groundwork.support.TestContext.getDriver;
 
@@ -17,8 +23,21 @@ public class BasePage {
         PageFactory.initElements(getDriver(), this);
     }
 
+    @FindBy(xpath="//div[contains(@class,'bx-type-overlay')]")
+    private List<WebElement> overlays;
+
     public void open() {
         getDriver().get(url);
+        closeOverlays();
+    }
+
+    public void closeOverlays() {
+        if (overlays.size() != 0) {
+            for (WebElement overlay : overlays.stream().filter(WebElement::isDisplayed).collect(Collectors.toList())) {
+                overlay.findElement(By.xpath(".//a[contains(@class,'bx-close-link')]")).click();
+            }
+            wait.until(driver -> overlays.stream().noneMatch(WebElement::isDisplayed));
+        }
     }
 
     public void waitToLoadWithJS() {
