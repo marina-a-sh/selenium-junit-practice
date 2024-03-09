a6203e42e3194126cf013b08d2b9360113383bc6
Marina A. Shaposhnik <1000324+marina-a-sh@users.noreply.github.com>
2024-03-09 00:06:08 -0800

    integrate with ReportPortal, add logback for logging
    Integrating this testing framework (currently JUnit4 based) with ReportPortal. ReportPortal is a web-based platform that unifies and analyzes test results from various platforms and types. https://reportportal.io/
    Targeted version of ReportPortal is 5.7.4, running locally on localhost:8080. Integration is via ServiceLoader mechanism.
    
    Switching to logback for logging to have SLF4J provider/binding that implements SLF4J api version 2.0.x that utilizes ServiceLoader mechanism.

--- src/test/java/org/groundwork/support/Loggable.java
+++ src/test/java/org/groundwork/support/Loggable.java
@@ -1,12 +1,14 @@
 package org.groundwork.support;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface Loggable {
 
+    //SLF4J (Simple Logging Facade for Java) is a simple facade or abstraction for various logging frameworks,
+    //such as Java Util Logging (JUL), Logback and Log4j2. https://howtodoinjava.com/logback/setting-up-slf4j/
     default Logger getLogger() {
-        return LogManager.getLogger(this.getClass());
+        return LoggerFactory.getLogger(this.getClass());
     }
 
 }
